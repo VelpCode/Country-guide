703a54327c702c0ed2cc155829e2c56a75e04a85
Aman Velpula <velpmail@gmail.com>
2023-01-08 18:49:42 -0500

    using fetch & then to attain attributes for js

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
 
-    let countryName = countryInp.value;
+    let countryName = "India";
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL)
-    fetch(finalURL).then((response) => response.json()) // response.json being fetched
+    fetch(finalURL).then((response) => response.json())
+    .then((data) => {
+        console.log(data[0]);
+        console.log(data[0].capital[0])
+        console.log(data[0].flags.svg)
+        console.log(data[0].name.common);
+        console.log(data[0].continents[0]);
+    })           // response.json being fetched
 });
